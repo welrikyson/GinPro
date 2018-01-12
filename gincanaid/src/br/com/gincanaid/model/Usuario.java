865410c3dad65443ad4b67c4314823cc3145937b
PROGRAMADOR-05 <PROGRAMADOR-05@DESKTOP-BQLP5N4>
2018-01-12 14:50:54 -0400

    criacao entidade usuario

---
+++ gincanaid/src/br/com/gincanaid/model/Usuario.java
@@ -0,0 +1,50 @@
+package br.com.gincanaid.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the usuario database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Usuario.findAll", query="SELECT u FROM Usuario u")
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+
+	private String login;
+
+	private String senha;
+
+	public Usuario() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return this.login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return this.senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
