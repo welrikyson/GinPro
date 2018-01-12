f6ba3dd43e612515f03b8c90d739c37788e7a32b
PROGRAMADOR-05 <PROGRAMADOR-05@DESKTOP-BQLP5N4>
2018-01-12 15:31:34 -0400

    classe de conexao com o banco de dados

---
+++ gincanaid/src/br/com/gincanaid/connection/Connection.java
@@ -0,0 +1,25 @@
+package br.com.gincanaid.connection;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Connection {
+	public static EntityManagerFactory emf;
+
+	public Connection() {
+		// TODO Auto-generated constructor stub
+		if (emf == null) {
+			System.out.println("criei a conexao");
+			emf = Persistence.createEntityManagerFactory("gincanaid");
+		} else
+			System.out.println("nao precisei de criar a conexao");
+	}
+
+	public EntityManagerFactory getConection() {
+		return emf;
+	}
+
+	public boolean connectionActiva() {
+		return emf.isOpen();
+	}
+}
