82211729b9e6f55158077a479b933f3206016e5f
PROGRAMADOR-05 <PROGRAMADOR-05@DESKTOP-BQLP5N4>
2018-01-16 11:35:58 -0400

    dashboard

---
+++ gincanaid/src/br/com/gincanaid/dao/EquipeDAO.java
@@ -0,0 +1,5 @@
+package br.com.gincanaid.dao;
+
+public class EquipeDAO{
+	
+}
---
+++ gincanaid/src/br/com/gincanaid/model/Equipe.java
@@ -0,0 +1,50 @@
+package br.com.gincanaid.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the Equipe database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Equipe.findAll", query="SELECT e FROM Equipe e")
+public class Equipe implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+
+	private String nome;
+
+	private int ponto;
+
+	public Equipe() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getPonto() {
+		return this.ponto;
+	}
+
+	public void setPonto(int ponto) {
+		this.ponto = ponto;
+	}
+
+}
